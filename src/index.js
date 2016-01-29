2e3e6a007e173c88499255549f80fd5d3d48cf50
Jaisen Mathai <jaisen@jmathai.com>
2016-01-29 00:31:27 -0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -10,11 +10,20 @@
  *    event.{pathParam}, Path parameters as defined in your .joule.yml
  *    event.{queryStringParam}, Query string parameters as defined in your .joule.yml
  */
+var client = require('twilio')(process.env.accountSid, process.env.authToken);
 exports.handler = function(event, context) {
   // take event and reverse all the values
+ 
+  client.messages.create({
+      body: event.body,
+      to: event.to,
+      from: process.env.from
+  }, function(err, message) {
+      context.succeed(message.sid);
+  });
   var output = {};
-  for(key in event) {
+  /*for(key in event) {
     output[key] = event[key].split('').reverse().join('');
   }
-  context.succeed(output);
+  context.succeed(output);*/
 };
